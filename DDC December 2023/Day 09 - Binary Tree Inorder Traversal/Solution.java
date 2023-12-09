161646e0a6fb2a90c65a84c99599ebc000ba41b7
theadityaroy <adityaroy2308@hotmail.com>
2023-12-09 22:22:10 +0530

    Initial Commit

---
+++ DDC December 2023/Day 09 - Binary Tree Inorder Traversal/Solution.java
@@ -0,0 +1,40 @@
+// Problem Link : https://leetcode.com/problems/binary-tree-inorder-traversal/description/
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        helper(root, res);
+        return res;
+    }
+
+    public void helper(TreeNode root, List<Integer> res) {
+        if (root != null) {
+            helper(root.left, res);
+            res.add(root.val);
+            helper(root.right, res);
+        }
+    }
+
+    // Ignore this section :
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
