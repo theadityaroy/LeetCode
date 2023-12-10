a73c14ab82d18243c15c5b585736602fb2fabbee
theadityaroy <adityaroy2308@hotmail.com>
2023-12-10 11:26:29 +0530

    Initial Commit

---
+++ DDC December 2023/Day 10 - Transpose Matrix/Solution.java
@@ -0,0 +1,17 @@
+// Problem Link : https://leetcode.com/problems/transpose-matrix/description/
+
+public class Solution {
+    public int[][] transpose(int[][] matrix) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+        int[][] res = new int[m][n];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                res[j][i] = matrix[i][j];
+            }
+        }
+
+        return res;
+    }
+}
---
+++ Top Interview 150/07 - Best Time to Buy and Sell Stock II/Solution.java
@@ -0,0 +1,15 @@
+// Problem Link : https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/description/
+
+public class Solution {
+    public int maxProfit(int[] prices) {
+        int profit = 0;
+
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i - 1] < prices[i]) {
+                profit += prices[i] - prices[i - 1];
+            }
+        }
+
+        return profit;
+    }
+}
