300c509f91fef52f313adca1d82ac9ac13c5a0c7
theadityaroy <adityaroy2308@hotmail.com>
2023-12-20 22:18:18 +0530

    Initial Commit

---
+++ DDC December 2023/Day 20 - Buy Two Chocolates/Solution.java
@@ -0,0 +1,20 @@
+// Problem Link : https://leetcode.com/problems/buy-two-chocolates/description/
+
+public class Solution {
+    public int buyChoco(int[] prices, int money) {
+        int min1 = Integer.MAX_VALUE;
+        int min2 = Integer.MAX_VALUE;
+
+        for (int price : prices) {
+            if (price < min1) {
+                min2 = min1;
+                min1 = price;
+            } else if (price < min2) {
+                min2 = price;
+            }
+        }
+
+        int remainingMoney = money - (min1 + min2);
+        return remainingMoney >= 0 ? remainingMoney : money;
+    }
+}
