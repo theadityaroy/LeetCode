16162982a161dee97869ea0f1d8d94bdd4f38795
theadityaroy <adityaroy2308@hotmail.com>
2024-01-01 13:02:34 +0530

    Initial Commit

---
+++ DCC January 2024/Day 01 - Assign Cookies/Solution.java
@@ -0,0 +1,26 @@
+// Problem Link : https://leetcode.com/problems/assign-cookies/description/
+
+import java.util.Arrays;
+
+public class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+
+        int count = 0;
+        int i = 0;
+        int j = 0;
+
+        while (i < g.length && j < s.length) {
+            if (g[i] <= s[j]) {
+                count++;
+                i++;
+                j++;
+            } else {
+                j++;
+            }
+        }
+
+        return count;
+    }
+}
