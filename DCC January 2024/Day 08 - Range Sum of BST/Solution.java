f80c51548ccc24724b823009e144686ce3bbe053
theadityaroy <adityaroy2308@hotmail.com>
2024-01-08 15:43:12 +0530

    Initial Commit

---
+++ DCC January 2024/Day 08 - Range Sum of BST/Solution.java
@@ -0,0 +1,36 @@
+// Problem Link : https://leetcode.com/problems/range-sum-of-bst/description/
+
+public class Solution {
+    public int rangeSumBST(TreeNode root, int low, int high) {
+        if (root == null) {
+            return 0;
+        }
+
+        int sum = 0;
+        if (root.val >= low && root.val <= high) {
+            sum += root.val;
+        }
+        if (root.val > low) {
+            sum += rangeSumBST(root.left, low, high);
+        }
+        if (root.val < high) {
+            sum += rangeSumBST(root.right, low, high);
+        }
+
+        return sum;
+    }
+
+    // Ignore this Section :
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
