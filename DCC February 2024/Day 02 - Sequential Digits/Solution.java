6118444f26a3298f353aeee1a07e0404f1a8a8ce
Aditya Roy <adityaroy2308@hotmail.com>
2024-02-02 18:30:06 +0530

    Initial Commit

---
+++ DCC February 2024/Day 02 - Sequential Digits/Solution.java
@@ -0,0 +1,23 @@
+// Problem Link : https://leetcode.com/problems/sequential-digits/description/
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    public List<Integer> sequentialDigits(int low, int high) {
+        String c = "123456789";
+        List<Integer> a = new ArrayList<>();
+
+        for (int i = 0; i < c.length(); i++) {
+            for (int j = i + 1; j <= c.length(); j++) {
+                int curr = Integer.parseInt(c.substring(i, j));
+                if (curr <= high && curr >= low) {
+                    a.add(curr);
+                }
+            }
+        }
+
+        a.sort(null);
+        return a;
+    }
+}
