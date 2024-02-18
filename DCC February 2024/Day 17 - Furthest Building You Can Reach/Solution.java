c88d737c95e462a88c9fccd467521c9f7fbc9ad0
Aditya Roy <adityaroy2308@hotmail.com>
2024-02-18 17:28:53 +0530

    Initial Commit

---
+++ DCC February 2024/Day 17 - Furthest Building You Can Reach/Solution.java
@@ -0,0 +1,24 @@
+// Problem Link : https://leetcode.com/problems/furthest-building-you-can-reach/description/
+
+import java.util.PriorityQueue;
+
+public class Solution {
+    public int furthestBuilding(int[] heights, int bricks, int ladders) {
+        PriorityQueue<Integer> heap = new PriorityQueue<>();
+        
+        for (int i = 0; i < heights.length - 1; i++) {
+            int diff = heights[i + 1] - heights[i];
+            if (diff > 0) {
+                heap.offer(diff);
+                if (heap.size() > ladders) {
+                    bricks -= heap.poll();
+                }
+                if (bricks < 0) {
+                    return i;
+                }
+            }
+        }
+        
+        return heights.length - 1;
+    }
+}
